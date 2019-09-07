3e821d3ce93d029483485406341ffb668f43d3a5
paul alvarez <palvarez@greenhouse.io>
2019-09-06 21:21:03 -0400

    Generalize script

--- generate_label_details.rb
+++ generate_label_details.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "pry-byebug"
 
 BANK_SIZE = Integer("4000", 16)
 
-OUT_FILE = "./pokecrystal-label-details"
 ROM_FILE = "./pokecrystal.gbc"
-# file should be a list of only the address labels you wanna compute -
-# basically a subset of pokecrystal.sym, in this case obtained by
-# grepping for "pclalv"
-DEFAULT_SYM_FILE = "./pokecrystal.original.address_labels"
+DEFAULT_SYM_FILE = "./pokecrystal.sym"
+OUT_FILE = "./pokecrystal-label-details.json"
 
 HIDDEN_ITEMS = ["MACHINE_PART"]
 ITEM_BALLS = %w(COIN_CASE HM_WATERFALL)
@@ -30,10 +28,15 @@ def rom
   @rom ||= IO.binread(ROM_FILE)
 end
 
+def byte_to_string(byte)
+  "$#{byte.ord.to_s(16)}"
+end
+
 def label_hex_addresses
   File
     .read(sym_file)
     .split("\n")
+    .select { |l| l.include?("ckir") }
     .map { |line| line.split(" ") }
     .map { |address, label| [label, address] }
 end
@@ -50,9 +53,9 @@ def main
   key_item_address_ranges =
     label_addresses
       .keys
-      .reject { |label| label.include?("POST") }
+      .reject { |label| label.include?("AFTER") }
       .map do |label|
-        post_label = label.gsub("pclalv", "pclalv_POST")
+        post_label = label.gsub("ckir_BEFORE", "ckir_AFTER")
         address_range = label_addresses[label]...label_addresses[post_label]
         key_item =
           if label.include?("RECV_")
@@ -71,49 +74,18 @@ def main
 
   key_item_details =
     key_item_address_ranges.map do |key_item, address_range|
-      case key_item
-      when *VERBOSE_GIVE_ITEMS
-        address = address_range.begin + 1
-        instruction = "verbosegiveitem"
-      when *GIVE_ITEMS
-        address = address_range.begin + 1
-        instruction = "giveitem"
-      when *ITEM_BALLS
-        address = address_range.begin
-        instruction = "itemball"
-      when *HIDDEN_ITEMS
-        address = address_range.last - 1 # all ranges are exclusive
-        instruction = "hiddenitem"
-      when *CHECK_EVENT
-        address = address_range.begin + 1
-        instruction = "checkevent"
-      when *SET_EVENT
-        address = address_range.begin + 1
-        instruction = "setevent"
-      when *IF_EQUAL_WEEKDAY
-        address = address_range.begin + 2
-        instruction = "ifequal"
-      else
-        puts "unknown key item #{key_item}; skipping"
-      end
-
       {
         "name" => key_item,
         "address_range" => { "begin" => address_range.begin, "end" => address_range.end },
-        "address" => address,
-        "value" => address && rom[address].ord,
-        "hex_value" => address && byte_to_string(rom[address]),
+        # "values" => rom[address_range], # it's difficult to write out raw bytes :(
+        "integer_values" => rom[address_range].split('').map(&:bytes).flatten.join(" "),
         "hex_values" => rom[address_range].each_char.map(&method(:byte_to_string)).join(" "),
-        "instruction" => instruction,
       }
     end.sort_by { |details| details["name"] }
 
-  File.write(OUT_FILE + ".json", key_item_details.to_json)
-  puts "generated #{OUT_FILE}.json"
-end
+  File.write(OUT_FILE, key_item_details.to_json)
 
-def byte_to_string(byte)
-  "$#{byte.ord.to_s(16)}"
+  puts "generated #{OUT_FILE}"
 end
 
 main
