d00ba14c95cc5bb500b6af77f505d344ee69a373
paul alvarez <pclalv@users.noreply.github.com>
2019-11-09 21:31:15 -0500

    Prevent hardlock from using Secretpotion early

--- maps/OlivineLighthouse6F.asm
+++ maps/OlivineLighthouse6F.asm
@@ -72,7 +72,7 @@ OlivineLighthouseJasmine:
 	;; can get the Cianwood Pharmacy item even if they've already
 	;; used the Secretpotion on Amphy; otherwise, the player could 
 	;; hardlock.
-	setevent EVENT_JASMINE_RETURNED_TO_GYM
+	setevent EVENT_JASMINE_EXPLAINED_AMPHYS_SICKNESS
 .ckir_AFTER_setevent_EVENT_JASMINE_RETURNED_TO_GYM::
 	clearevent EVENT_OLIVINE_GYM_JASMINE
 	readvar VAR_FACING
